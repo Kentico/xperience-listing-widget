7462b52cc86062bc93fc415a60537a513523c026
Hana Pospisilova <HanaP@kentico.com>
2021-12-15 17:38:50 +0100

    KXA-46 create page type selector and page view component tests

--- DancingGoatCore/Components/Widgets/ListingWidget/ListingWidgetViewModel.cs
+++ DancingGoatCore/Components/Widgets/ListingWidget/ListingWidgetViewModel.cs
@@ -11,5 +11,17 @@ namespace DancingGoat.Widgets
         /// Pages to show.
         /// </summary>
         public IEnumerable<ListingWidgetPageViewModel> Pages { get; set; }
+
+
+        /// <summary>
+        /// All supported page types.
+        /// </summary>
+        public IEnumerable<string> SupportedPageTypes { get; set; }
+
+
+        /// <summary>
+        /// Name of the selected page type to display.
+        /// </summary>
+        public string SelectedPageType { get; set; }
     }
 }
---
+++ Tests/Components/Widgets/ListingWidget/ListingWidgetPageViewComponentTests.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+using CMS.DocumentEngine;
+using CMS.DocumentEngine.Types.DancingGoatCore;
+using CMS.Tests;
+
+using Kentico.Content.Web.Mvc;
+using Kentico.PageBuilder.Web.Mvc;
+
+using DancingGoat.Models;
+using DancingGoat.Widgets;
+
+using Tests.DocumentEngine;
+
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DancingGoatCore.Widgets.Tests
+{
+    class ListingWidgetPageViewComponentTests : UnitTests
+    {
+        private const string VIEW_NAME = "~/Components/Widgets/ListingWidget/_ListingWidget.cshtml";
+
+        private PageRepository pageRepository;
+        private ListingWidgetViewComponent listingWidgetViewComponent;
+        private ComponentViewModel<ListingWidgetProperties> componentViewModel;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            pageRepository = Substitute.ForPartsOf<PageRepository>(Substitute.For<IPageRetriever>());
+            listingWidgetViewComponent = new ListingWidgetViewComponent(pageRepository);
+
+            var page = Substitute.For<TreeNode>();
+            var properties = Substitute.For<ListingWidgetProperties>();
+            componentViewModel = ComponentViewModel<ListingWidgetProperties>.Create(page, properties);
+        }
+
+
+        [Test]
+        public void Invoke_RepositoryWithoutPages_ReturnsCorrectViewModelWithoutPages()
+        {
+            var viewResult = listingWidgetViewComponent.Invoke(componentViewModel) as ViewViewComponentResult;
+            var viewModel = viewResult.ViewData.Model as ListingWidgetViewModel;
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(viewResult.ViewName, Is.EqualTo(VIEW_NAME));
+                Assert.That(viewModel.Pages.Count(), Is.EqualTo(0));
+                Assert.That(viewModel.SupportedPageTypes, Is.EqualTo(ListingWidgetProperties.SupportedPageTypes));
+                Assert.That(viewModel.SelectedPageType, Is.EqualTo(null));
+            });
+        }
+
+
+        [Test]
+        public void Invoke_RepositoryWithTwoPages_ReturnsCorrectViewModelWithTwoPages()
+        {
+            Fake().DocumentType<Coffee>(Coffee.CLASS_NAME);
+            Fake().DocumentType<Article>(Article.CLASS_NAME);
+            var pagesList = new List<TreeNode> { TreeNode.New<Article>(), TreeNode.New<Coffee>() };
+            pageRepository.GetAllPages<TreeNode>().Returns(pagesList);
+
+            var viewResult = listingWidgetViewComponent.Invoke(componentViewModel) as ViewViewComponentResult;
+            var viewModel = viewResult.ViewData.Model as ListingWidgetViewModel;
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(viewResult.ViewName, Is.EqualTo(VIEW_NAME));
+                Assert.That(viewModel.Pages.Count(), Is.EqualTo(2));
+                Assert.That(viewModel.SupportedPageTypes, Is.EqualTo(ListingWidgetProperties.SupportedPageTypes));
+                Assert.That(viewModel.SelectedPageType, Is.EqualTo(null));
+            });
+        }
+
+
+        [Test]
+        public void Invoke_PropertiesWithSelectedPageType_ReturnsCorrectViewModelWithSelectedPageType()
+        {
+            var testPageType = "Article";
+            componentViewModel.Properties.SelectedPageType = testPageType;
+
+            var viewResult = listingWidgetViewComponent.Invoke(componentViewModel) as ViewViewComponentResult;
+            var viewModel = viewResult.ViewData.Model as ListingWidgetViewModel;
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(viewResult.ViewName, Is.EqualTo(VIEW_NAME));
+                Assert.That(viewModel.Pages.Count(), Is.EqualTo(0));
+                Assert.That(viewModel.SupportedPageTypes, Is.EqualTo(ListingWidgetProperties.SupportedPageTypes));
+                Assert.That(viewModel.SelectedPageType, Is.EqualTo(testPageType));
+            });
+        }
+
+    }
+}
