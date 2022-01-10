66bd7b08fc55f636c3134c59be09179f358a4af6
Hana Pospisilova <HanaP@kentico.com>
2022-01-10 13:29:59 +0100

    KXA-46 change order of parameters of dropdown editor view model

--- DancingGoatCore/Components/InlineEditors/DropdownEditor/DropdownEditorViewModel.cs
+++ DancingGoatCore/Components/InlineEditors/DropdownEditor/DropdownEditorViewModel.cs
@@ -10,15 +10,15 @@ namespace DancingGoat.InlineEditors
     public class DropdownEditorViewModel : InlineEditorViewModel
     {
         /// <summary>
-        /// Selected option.
-        /// </summary>
-        public string SelectedOption { get; set; }
-
-
-        /// <summary>
         /// Select list for all options of the selector.
         /// </summary>
         public SelectList Options { get; set; }
+
+
+        /// <summary>
+        /// Selected option.
+        /// </summary>
+        public string SelectedOption { get; set; }
 
 
         /// <summary>
@@ -31,14 +31,14 @@ namespace DancingGoat.InlineEditors
         /// Creates an instance of <see cref="DropdownEditorViewModel"/> class.
         /// </summary>
         /// <param name="propertyName">Name of related widget property.</param>
-        /// <param name="selectedOption">Selected option.</param>
         /// <param name="options">All options of the selector.</param> 
+        /// <param name="selectedOption">Selected option.</param>
         /// <param name="label">Label of the selector.</param> 
-        public DropdownEditorViewModel(string propertyName, string selectedOption, IEnumerable<DropdownOptionViewModel> options, string label)
+        public DropdownEditorViewModel(string propertyName, IEnumerable<DropdownOptionViewModel> options, string selectedOption, string label)
         {
             PropertyName = propertyName;
-            SelectedOption = selectedOption;
             Options = new SelectList(options, nameof(DropdownOptionViewModel.Value), nameof(DropdownOptionViewModel.Text));
+            SelectedOption = selectedOption;
             Label = label;
         }
     }
