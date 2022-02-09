83899af3b7f0e8bb7e82dc45b5a76f76fbbf4f73
Hana Pospisilova <HanaP@kentico.com>
2022-02-09 17:23:50 +0100

    KXA-49 fix comments of ITransformationService methods

--- DancingGoatCore/Components/Widgets/ListingWidget/Transformations/ITransformationService.cs
+++ DancingGoatCore/Components/Widgets/ListingWidget/Transformations/ITransformationService.cs
@@ -25,28 +25,28 @@ namespace DancingGoat.Widgets
 
 
         /// <summary>
-        /// Returns hydrated <see cref="ITransformationViewModel"/> for pages to be used in corresponding views.
+        /// Returns hydrated <see cref="ITransformationViewModel"/> with pages for corresponding views.
         /// </summary>
-        /// <param name="pages">Pages to be listed in view model.</param>
+        /// <param name="pages">Pages to be used in view model.</param>
         ITransformationViewModel GetModel(IEnumerable<TreeNode> pages);
 
 
         /// <summary>
-        /// Returns Action for applying custom cache dependency for page retriever to get pages which will be listed in the corresponding transformation view.
+        /// Returns Action for applying custom cache dependency for page retriever to retrieve pages for corresponding view.
         /// </summary>
         /// <param name="transformationView">Transformation view path.</param>
         Action<IPageCacheDependencyBuilder<TreeNode>, IEnumerable<TreeNode>> GetCustomCacheDependency(string transformationView);
 
 
         /// <summary>
-        /// Returns custom cache dependency key for page retriever to get pages which will be listed in the corresponding transformation view.
+        /// Returns custom cache dependency key for page retriever to retrieve pages for corresponding view.
         /// </summary>
         /// <param name="transformationView">Transformation view path.</param>
         string GetCustomCacheDependencyKey(string transformationView);
 
 
         /// <summary>
-        /// Returns Action for applying custom query parameters for page retriever to get pages which will be listed in the corresponding transformation view.
+        /// Returns Action for applying custom query parameters for page retriever to retrieve pages for corresponding view.
         /// </summary>
         /// <param name="transformationView">Transformation view path.</param>
         Action<DocumentQuery> GetCustomQueryParametrization(string transformationView);
